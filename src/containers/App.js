01aede5fbf403f2f562c18c66b32ccd18979c97c
tomas95go <tomas95go@gmail.com>
2018-10-19 16:58:44 -0300

    Deploying the robofriends app/Subiendo la app robofriends

---
+++ src/containers/App.js
@@ -0,0 +1,66 @@
+import React, {Component} from 'react';
+import CardList from '../components/CardList';
+import Searchbox from '../components/Searchbox';
+import Scroll from '../components/Scroll';
+import './App.css';
+
+class App extends Component {
+
+	constructor(){
+		super()
+
+		this.state = {
+
+			robots: [],
+
+			searchField: ''
+
+		}
+
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => {this.setState({robots: users})});
+
+	}
+
+	onSearchChange = (event) => {
+
+		this.setState({ searchField: event.target.value })
+
+	}
+
+	render() {
+
+		const { robots, searchField } = this.state;
+
+		const filterRobots = robots.filter(robot =>{
+
+			return robot.name.toLowerCase().includes(searchField.toLowerCase());
+
+		})
+
+		return !robots.length ?
+
+			<h1>Loading...</h1> :
+
+				<div className="tc">
+
+					<h1 className='f1'> RoboFriends </h1>
+
+					<Searchbox searchChange={this.onSearchChange}/>
+
+					<Scroll>
+
+						<CardList robots={filterRobots } />
+
+					</Scroll>	
+
+				</div>
+			}
+			
+	}
+
+export default App;
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Robotron',
+	username: 'Robotrondabest',
+	email: 'robotrondabest@gmail.com'
+},
+{
+	id: 2,
+	name: 'Robobest',
+	username: 'Robest',
+	email: 'robest@gmail.com'
+},
+{
+	id: 3,
+	name: 'Megatron',
+	username: 'Megatron123',
+	email: 'megratron@gmail.com'
+},
+{
+	id: 4,
+	name: 'Sapphire',
+	username: 'Sap',
+	email: 'sap@gmail.com'
+},
+{
+	id: 5,
+	name: 'Dudley',
+	username: 'megaDudley',
+	email: 'dudley@gmail.com'
+},
+{
+	id: 6,
+	name: 'Balrgo',
+	username: 'Bal',
+	email: 'balbal@gmail.com'
+},
+{
+	id: 7,
+	name: 'Bob Marley',
+	username: 'bMarley',
+	email: 'bmarley@gmail.com'
+},
+{
+	id: 8,
+	name: 'Token',
+	username: 'tokenR',
+	email: 'token@gmail.com'
+},
+{
+	id: 9,
+	name: 'key',
+	username: 'keyS',
+	email: 'key@gmail.com'
+},
+{
+	id: 10,
+	name: 'Sin Azucar',
+	username: 'sinAzucar',
+	email: 'sinazucar@gmail.com'
+},
+]
