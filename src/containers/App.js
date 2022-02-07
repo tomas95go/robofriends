bb65bd87dad7bee0e11151d9f97405eef376dc21
Tomas Gonzalez Oviedo <tomas95go@gmail.com>
2022-02-07 01:31:57 -0300

    refactor(App.js): Implementing useState hook for the search

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,66 +1,18 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import CardList from '../components/CardList';
 import Searchbox from '../components/Searchbox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
-class App extends Component {
+function App() {
+  const [searchTerm, setSearchTerm] = useState('');
 
-	constructor(){
-		super()
-
-		this.state = {
-
-			robots: [],
-
-			searchField: ''
-
-		}
-
-	}
-
-	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then(response => response.json())
-			.then(users => {this.setState({robots: users})});
-
-	}
-
-	onSearchChange = (event) => {
-
-		this.setState({ searchField: event.target.value })
-
-	}
-
-	render() {
-
-		const { robots, searchField } = this.state;
-
-		const filterRobots = robots.filter(robot =>{
-
-			return robot.name.toLowerCase().includes(searchField.toLowerCase());
-
-		})
-
-		return !robots.length ?
-
-			<h1>Cargando...</h1> :
-
-				<div className="tc">
-
-					<h1 className='f1'> Buscador dinamico hecho con React </h1>
-
-					<Searchbox searchChange={this.onSearchChange}/>
-
-					<Scroll>
-
-						<CardList robots={filterRobots } />
-
-					</Scroll>	
-
-				</div>
-			}
-			
-	}
+  return (
+    <div>
+      <h1>Hooks working my dude!</h1>
+      <Searchbox searchChange={(e) => setSearchTerm(e.target.value)} />
+    </div>
+  );
+}
 
 export default App;
