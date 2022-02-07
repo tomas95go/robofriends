e4d1a0004fcb66b29d228b79723f4e555daed430
Tomas Gonzalez Oviedo <tomas95go@gmail.com>
2022-02-07 02:01:41 -0300

    refactor(App.js): Migrated fetch call to hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import Searchbox from '../components/Searchbox';
 import Scroll from '../components/Scroll';
@@ -6,11 +6,29 @@ import './App.css';
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
+  const [robots, setRobots] = useState([]);
 
-  return (
-    <div>
-      <h1>Hooks working my dude!</h1>
+  const filterRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => {
+        setRobots(users);
+      });
+  }, []);
+
+  return !robots.length ? (
+    <h1>Loading...</h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1"> Search your buddy! </h1>
       <Searchbox searchChange={(e) => setSearchTerm(e.target.value)} />
+      <Scroll>
+        <CardList robots={filterRobots} />
+      </Scroll>
     </div>
   );
 }
